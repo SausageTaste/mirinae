be29bd54b139b6f479c8571a1f5c955dba777b5c
Sungmin Woo <woos8899@gmail.com>
2023-07-04 16:56:45 +0900

    Use GLFW for window creation

--- src/engine.cpp
+++ src/engine.cpp
@@ -1,18 +1,59 @@
 #include "mirinae/engine.hpp"
 
+#include <cassert>
+#include <stdexcept>
+
+#include <GLFW/glfw3.h>
+
+namespace {
+
+    class GlfwRaii {
+    public:
+        GlfwRaii() { glfwInit(); }
+        ~GlfwRaii() { glfwTerminate(); }
+    } g_glfw_raii;
+
+
+    class EngineGlfw : public mirinae::IEngine {
+
+    public:
+        EngineGlfw() {
+            this->window = glfwCreateWindow(640, 480, "My Title", nullptr, nullptr);
+            assert(nullptr != this->window);
+        }
+
+        ~EngineGlfw() {
+            this->destroy();
+        }
+
+        void destroy() {
+            if (nullptr != this->window) {
+                glfwDestroyWindow(this->window);
+                this->window = nullptr;
+            }
+        }
+
+        void do_frame() override {
+            glfwSwapBuffers(this->window);
+            glfwPollEvents();
+        }
+
+        bool is_ongoing() override {
+            return !glfwWindowShouldClose(this->window);
+        }
+
+    private:
+        GLFWwindow* window = nullptr;
+
+    };
+
+}
+
 
 namespace mirinae {
 
-    Engine::Engine() {
-
-    }
-
-    void Engine::do_frame() {
-
-    }
-
-    bool Engine::is_ongoing() {
-        return true;
+    std::unique_ptr<IEngine> create_engine() {
+        return std::make_unique<EngineGlfw>();
     }
 
 }
