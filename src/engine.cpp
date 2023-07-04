c90cddc74111f2f56940a23d1c64a8444fef6d3b
Sungmin Woo <woos8899@gmail.com>
2023-07-04 13:23:13 +0900

    Basic project structure

---
+++ src/engine.cpp
@@ -0,0 +1,18 @@
+#include "mirinae/engine.hpp"
+
+
+namespace mirinae {
+
+    Engine::Engine() {
+
+    }
+
+    void Engine::do_frame() {
+
+    }
+
+    bool Engine::is_ongoing() {
+        return true;
+    }
+
+}
