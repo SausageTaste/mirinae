4c27000979a948c2047d09e30ab4920098b0d92c
Sungmin Woo <woos8899@gmail.com>
2023-07-05 14:57:27 +0900

    Some OpenGL primitive classes

---
+++ include/mirinae/render/glprimitive.hpp
@@ -0,0 +1,117 @@
+#pragma once
+
+#include <stdexcept>
+
+#include <spdlog/spdlog.h>
+#include <glad/gl.h>
+
+
+namespace mirinae {
+
+    void log_gl_error() {
+        const auto err = glGetError();
+        if (GL_NO_ERROR != err) {
+            spdlog::warn("OpenGL error: {}", err);
+        }
+    }
+
+
+    class BufferObject {
+
+    public:
+        ~BufferObject() {
+            this->destroy();
+        }
+
+        void init(const void* data, size_t size) {
+            glGenBuffers(1, &this->handle);
+            glBindBuffer(GL_ARRAY_BUFFER, this->handle);
+            glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+            log_gl_error();
+        }
+
+        void destroy() {
+            if (0 != this->handle) {
+                glDeleteBuffers(1, &this->handle);
+                this->handle = 0;
+            }
+        }
+
+        bool is_ready() const {
+            return this->handle != 0;
+        }
+
+    private:
+        GLuint handle = 0;
+
+    };
+
+
+    class ShaderUnit {
+
+    public:
+        enum class Type {
+            vertex,
+            fragment,
+        };
+
+    public:
+        ShaderUnit(Type type, const char* source) {
+            this->handle = glCreateShader(this->interpret_shader_type(type));
+            if (0 == this->handle) {
+                log_gl_error();
+                return;
+            }
+
+            glShaderSource(this->handle, 1, &source, nullptr);
+            glCompileShader(this->handle);
+
+            if (!this->is_success()) {
+                char msg[512];
+                glGetShaderInfoLog(this->handle, 512, nullptr, msg);
+                spdlog::error("Shader compilation failed...\n{}", msg);
+            }
+        }
+
+        ~ShaderUnit() {
+            this->destroy();
+        }
+
+        void destroy() {
+            if (0 != this->handle) {
+                glDeleteShader(this->handle);
+                this->handle = 0;
+            }
+        }
+
+        bool is_ready() const {
+            return this->is_success();
+        }
+
+    private:
+        bool is_success() const {
+            if (0 == this->handle)
+                return false;
+
+            int success;
+            glGetShaderiv(this->handle, GL_COMPILE_STATUS, &success);
+            return 0 != success;
+        }
+
+        static GLenum interpret_shader_type(Type type) {
+            switch (type) {
+                case Type::vertex:
+                    return GL_VERTEX_SHADER;
+                case Type::fragment:
+                    return GL_FRAGMENT_SHADER;
+                default:
+                    spdlog::error("Unknown ShaderUnit::Type value: {}", static_cast<int>(type));
+                    throw std::runtime_error{""};
+            }
+        }
+
+        GLuint handle = 0;
+
+    };
+
+}
