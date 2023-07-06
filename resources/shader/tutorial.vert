1e41f781a7edac28f717fef092ea4b885dd4928f
Sungmin Woo <woos8899@gmail.com>
2023-07-06 15:43:18 +0900

    Uniform and camera

--- resources/shader/tutorial.vert
+++ resources/shader/tutorial.vert
@@ -1,7 +1,11 @@
 #version 450 core
 
-layout (location = 0) in vec3 aPos;
+layout (location = 0) in vec3 i_pos;
+
+uniform mat4 u_proj_mat;
+
 
 void main() {
-    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    vec4 world_pos = u_proj_mat * vec4(i_pos, 1);
+    gl_Position = world_pos;
 }
--- src/mirinae/engine.cpp
+++ src/mirinae/engine.cpp
@@ -7,6 +7,10 @@
 #include <spdlog/spdlog.h>
 #include <glad/gl.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 #include <daltools/util.h>
 
@@ -78,6 +82,31 @@ namespace {
     };
 
 
+    class QuatCamera {
+
+    public:
+        void rotate(const float angle, const glm::vec3& axis) {
+            this->quat = this->rotate_quat(this->quat, angle, axis);
+        }
+
+        glm::mat4 make_view_mat() const {
+            const auto r = glm::toMat4(this->quat);
+            const auto t = glm::translate(r, -this->pos);
+            return t;
+        }
+
+        glm::vec3 pos{};
+
+    private:
+        glm::quat rotate_quat(const glm::quat& q, const float angle, const glm::vec3& axis) {
+            return glm::normalize(glm::angleAxis(angle, axis) * q);
+        }
+
+        glm::quat quat{};
+
+    };
+
+
     class EngineGlfw : public mirinae::IEngine {
 
     public:
@@ -85,9 +114,9 @@ namespace {
             : window(this)
         {
             float vertices[] = {
-                -0.5f, -0.5f, 0.0f,
-                0.5f, -0.5f, 0.0f,
-                0.0f,  0.5f, 0.0f
+                -1, -0.5 * std::sqrt(3.0), 0,
+                 1, -0.5 * std::sqrt(3.0), 0,
+                 0,  0.5 * std::sqrt(3.0), 0
             };
             vbo.init(vertices, sizeof(vertices));
             vao.init(vbo);
@@ -98,14 +127,29 @@ namespace {
             mirinae::ShaderUnit vertex_shader{ mirinae::ShaderUnit::Type::vertex, vertex_src->c_str() };
             mirinae::ShaderUnit fragment_shader{ mirinae::ShaderUnit::Type::fragment, fragment_src->c_str() };
             this->program.init(vertex_shader, fragment_shader);
+
+            this->camera.pos.z = 5;
+
+            this->on_window_resize(640, 480);
         }
 
         void do_frame() override {
+            // Update scene
+
+            const auto delta_time = this->timer.check_get_elapsed();
+            this->camera.rotate(delta_time, glm::vec3{0, 0, 1});
+
+            // Render
+
             const auto sec = dal::get_cur_sec();
-            glClearColor(std::sin(sec * 0.3f), std::sin(sec * 1.7f), std::sin(sec * 3.4f), 1);
+            glClearColor(std::sin(sec * 0.3), std::sin(sec * 1.7), std::sin(sec * 3.4), 1);
             glClear(GL_COLOR_BUFFER_BIT);
 
             this->program.use();
+
+            const auto mat = this->proj_mat * this->camera.make_view_mat();
+            glUniformMatrix4fv(this->program.get_uniform_loc("u_proj_mat"), 1, GL_FALSE, glm::value_ptr(mat));
+
             this->vao.use();
             glDrawArrays(GL_TRIANGLES, 0, 3);
             mirinae::log_gl_error("do_frame");
@@ -120,13 +164,18 @@ namespace {
 
         void on_window_resize(unsigned width, unsigned height) override {
             glViewport(0, 0, width, height);
+            this->proj_mat = glm::perspective<float>(90, static_cast<double>(width) / static_cast<double>(height), 0.1, 100);
+            spdlog::info("Window resize: {}x{}", width, height);
         }
 
     private:
         GlfwWindow window;
+        dal::Timer timer;
         mirinae::BufferObject vbo;
         mirinae::VertexArrayObject vao;
         mirinae::ShaderProgram program;
+        glm::mat4 proj_mat;
+        QuatCamera camera;
 
     };
 
