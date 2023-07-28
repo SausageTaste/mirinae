6b7a4b31e05b85c99539f7d92069d2a77acf0d29
Sungmin Woo <woos8899@gmail.com>
2023-07-28 16:24:17 +0900

    Shader compile script

---
+++ script/compile_shaders.py
@@ -0,0 +1,41 @@
+import os
+
+import utils
+
+
+def __try_mkdir(path: str):
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        pass
+
+
+def __create_shader_folder(root_dir: str):
+    __try_mkdir(os.path.join(root_dir, "resources"))
+    __try_mkdir(os.path.join(root_dir, "resources", "shaders"))
+
+
+def main():
+    vulkan_sdk_dir = utils.find_vulkan_sdk();
+    root_dir = utils.find_root_dir()
+    glsl_dir = os.path.join(root_dir, "resource_source", "glsl")
+    compiler_path = os.path.join(vulkan_sdk_dir, "Bin", "glslc.exe")
+    if not os.path.isfile(compiler_path):
+        raise FileNotFoundError("GLSL compiler not found at '{}'".format(compiler_path))
+
+    for glsl_filename_ext in os.listdir(glsl_dir):
+        glsl_filename, glsl_ext = os.path.splitext(glsl_filename_ext)
+        glsl_path = os.path.join(glsl_dir, glsl_filename_ext)
+
+        if os.path.isfile(glsl_path):
+            output_filename_ext = "{}_{}.spv".format(glsl_filename, glsl_ext.strip("."))
+            output_path = os.path.join(root_dir, "resources", "shaders", output_filename_ext)
+            cmd = f'{compiler_path} "{glsl_path}" -o "{output_path}"'
+
+            print(cmd)
+            __create_shader_folder(root_dir)
+            os.system(cmd)
+
+
+if "__main__" == __name__:
+    main()
---
+++ script/utils.py
@@ -0,0 +1,41 @@
+import os
+from typing import Optional
+
+
+def _is_dir_root(path: str):
+    if not os.path.isdir(path):
+        return False
+
+    required_contents = {
+        "resources",
+        ".gitignore",
+        "README.md",
+    }
+    contents = set(os.listdir(path));
+    diff = required_contents.difference(contents)
+    return 0 == len(diff)
+
+
+def find_root_dir() -> Optional[str]:
+    cur_dir = "."
+    for i in range(10):
+        if _is_dir_root(cur_dir):
+            return os.path.normpath(cur_dir)
+        else:
+            cur_dir = os.path.join(cur_dir, "..")
+
+    return None
+
+
+def find_vulkan_sdk() -> Optional[str]:
+    VULKAN_SDK_DIR = "C:/VulkanSDK"
+
+    if not os.path.isdir(VULKAN_SDK_DIR):
+        return False
+
+    for version_folder_name in reversed(os.listdir(VULKAN_SDK_DIR)):
+        sdk_version_folder = os.path.join(VULKAN_SDK_DIR, version_folder_name)
+        if os.path.isdir(sdk_version_folder):
+            return os.path.normpath(sdk_version_folder)
+
+    return None
