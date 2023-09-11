ecf7992f33d95c873ecc20695572d8a4d600777a
Sungmin Woo <woos8899@gmail.com>
2023-09-11 15:16:46 +0900

    Tidy inclusions

--- include/mirinae/render/uniform.hpp
+++ include/mirinae/render/uniform.hpp
@@ -1,11 +1,8 @@
 #pragma once
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
+#include "mirinae/util/include_glm.hpp"
 
-#include "mirinae/render/vkmajorplayers.hpp"
+#include "vkmajorplayers.hpp"
 
 
 namespace mirinae {
