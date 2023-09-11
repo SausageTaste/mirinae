7cde16176a06276ad52056eeef83382423e09d55
Sungmin Woo <woos8899@gmail.com>
2023-09-11 15:34:39 +0900

    Implementations into cpp file

--- include/mirinae/util/mamath.hpp
+++ include/mirinae/util/mamath.hpp
@@ -5,30 +5,16 @@
 
 namespace mirinae {
 
-    inline glm::quat rotate_quat(const glm::quat& q, const float angle, const glm::vec3& axis) {
-        return glm::normalize(glm::angleAxis(angle, axis) * q);
-    }
+    glm::quat rotate_quat(const glm::quat& q, const float angle, const glm::vec3& axis);
 
 
     class TransformQuat {
 
     public:
-        void rotate(const float angle, const glm::vec3& axis) {
-            rot_ = rotate_quat(rot_, angle, axis);
-        }
+        void rotate(const float angle, const glm::vec3& axis);
 
-        glm::mat4 make_model_mat() const {
-            const auto rot_mat = glm::mat4_cast(rot_);
-            const auto scale_mat = glm::scale(glm::mat4(1.0f), scale_);
-            const auto translate_mat = glm::translate(glm::mat4(1.0f), pos_);
-            return translate_mat * rot_mat * scale_mat;
-        }
-
-        glm::mat4 make_view_mat() const {
-            const auto rot_mat = glm::mat4_cast(glm::conjugate(rot_));
-            const auto translate_mat = glm::translate(glm::mat4(1.0f), -pos_);
-            return rot_mat * translate_mat;
-        }
+        glm::mat4 make_model_mat() const;
+        glm::mat4 make_view_mat() const;
 
         glm::quat rot_{ 1, 0, 0, 0 };
         glm::vec3 pos_{ 0, 0, 0 };
--- src/mirinae/util/mamath.cpp
+++ src/mirinae/util/mamath.cpp
@@ -1,7 +1,33 @@
 #include "mirinae/util/mamath.hpp"
 
 
+namespace mirinae {
+
+    glm::quat rotate_quat(const glm::quat& q, const float angle, const glm::vec3& axis) {
+        return glm::normalize(glm::angleAxis(angle, axis) * q);
+    }
+
+}
+
+
 // TransformQuat
 namespace mirinae {
+
+    void TransformQuat::rotate(const float angle, const glm::vec3& axis) {
+        rot_ = rotate_quat(rot_, angle, axis);
+    }
+
+    glm::mat4 TransformQuat::make_model_mat() const {
+        const auto rot_mat = glm::mat4_cast(rot_);
+        const auto scale_mat = glm::scale(glm::mat4(1.0f), scale_);
+        const auto translate_mat = glm::translate(glm::mat4(1.0f), pos_);
+        return translate_mat * rot_mat * scale_mat;
+    }
+
+    glm::mat4 TransformQuat::make_view_mat() const {
+        const auto rot_mat = glm::mat4_cast(glm::conjugate(rot_));
+        const auto translate_mat = glm::translate(glm::mat4(1.0f), -pos_);
+        return rot_mat * translate_mat;
+    }
 
 }
