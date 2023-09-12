40b5a9681d505811f68597e2ede979d960030a23
Sungmin Woo <woos8899@gmail.com>
2023-09-12 17:39:52 +0900

    Depth testing

--- src/mirinae/render/pipeline.cpp
+++ src/mirinae/render/pipeline.cpp
@@ -186,6 +186,20 @@ namespace mirinae {
             multisampling.alphaToOneEnable = VK_FALSE;
         }
 
+        VkPipelineDepthStencilStateCreateInfo depthStencil{};
+        {
+            depthStencil.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
+            depthStencil.depthTestEnable = VK_TRUE;
+            depthStencil.depthWriteEnable = VK_TRUE;
+            depthStencil.depthCompareOp = VK_COMPARE_OP_LESS;
+            depthStencil.depthBoundsTestEnable = VK_FALSE;
+            depthStencil.minDepthBounds = 0;
+            depthStencil.maxDepthBounds = 0;
+            depthStencil.stencilTestEnable = VK_FALSE;
+            depthStencil.front = {};
+            depthStencil.back = {};
+        }
+
         VkPipelineColorBlendAttachmentState colorBlendAttachment{};
         {
             colorBlendAttachment.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT;
@@ -238,7 +252,7 @@ namespace mirinae {
         pipelineInfo.pViewportState = &viewportState;
         pipelineInfo.pRasterizationState = &rasterizer;
         pipelineInfo.pMultisampleState = &multisampling;
-        pipelineInfo.pDepthStencilState = nullptr;
+        pipelineInfo.pDepthStencilState = &depthStencil;
         pipelineInfo.pColorBlendState = &colorBlending;
         pipelineInfo.pDynamicState = &dynamicState;
         pipelineInfo.layout = pipelineLayout;
