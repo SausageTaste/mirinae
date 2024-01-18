bc760c2af067e821fd6fd9db557128c8c4bde90f
Sungmin Woo <woos8899@gmail.com>
2024-01-18 17:09:37 +0900

    Simple overlay render pass

--- lib/core/include/mirinae/render/renderpass.hpp
+++ lib/core/include/mirinae/render/renderpass.hpp
@@ -77,4 +77,13 @@ namespace mirinae {
         VulkanDevice& device
     );
 
+    std::unique_ptr<IRenderPassBundle> create_overlay(
+        uint32_t width,
+        uint32_t height,
+        FbufImageBundle& fbuf_bundle,
+        DesclayoutManager& desclayouts,
+        Swapchain& swapchain,
+        VulkanDevice& device
+    );
+
 }
