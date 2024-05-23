7073182afc5cac88a509f8b4e8071a166111a0ab
Sungmin Woo <woos8899@gmail.com>
2024-05-23 16:37:09 +0900

    Load static models via Lua

---
+++ lib/core/src/mirinae/scene/scene.cpp
@@ -0,0 +1,72 @@
+#include "mirinae/scene/scene.hpp"
+
+#include "mirinae/scene/transform.hpp"
+
+
+namespace {
+
+    mirinae::Scene* find_scene_ptr(lua_State* const L) {
+        const auto usrptr = mirinae::find_global_ptr(L, "__mirinae_scene_ptr");
+        const auto scene = static_cast<mirinae::Scene*>(usrptr);
+        if (!scene)
+            return nullptr;
+        if (scene->magic_num_ != mirinae::Scene::MAGIC_NUM)
+            return nullptr;
+        return scene;
+    }
+
+}  // namespace
+
+
+namespace { namespace scene {
+
+    int create_static_model(lua_State* const L) {
+        const auto scene = ::find_scene_ptr(L);
+        if (!scene)
+            return luaL_error(L, "Scene pointer not found");
+
+        const auto model_path = luaL_checkstring(L, 1);
+
+        {
+            const auto enttid = scene->reg_.create();
+            scene->entt_without_model_.push_back(enttid);
+
+            auto& mactor = scene->reg_.emplace<mirinae::cpnt::StaticModelActor>(
+                enttid
+            );
+            mactor.model_path_.assign(model_path);
+
+            auto& trans = scene->reg_.emplace<mirinae::cpnt::Transform>(enttid);
+            trans.scale_ = glm::vec3{ 0.01f, 0.01f, 0.01f };
+        }
+
+        // scene = require('scene')
+        // scene.create_static_model("asset/models/sponza/sponza.dmd")
+        return 0;
+    }
+
+
+    int create_skinned_model(lua_State* const L) {}
+
+
+    int luaopen_scene(lua_State* const L) {
+        {
+            mirinae::LuaFuncList funcs;
+            funcs.add("create_static_model", create_static_model);
+            luaL_newlib(L, funcs.data());
+        }
+
+        return 1;
+    }
+
+}}  // namespace ::scene
+
+
+namespace mirinae {
+
+    Scene::Scene(ScriptEngine& script) : script_(script) {
+        script_.register_global_ptr("__mirinae_scene_ptr", this);
+        script_.register_module("scene", ::scene::luaopen_scene);
+    }
+
+}  // namespace mirinae
