78dcf764459f906cb927e781dc86dd73eee22c7a
Sungmin Woo <woos8899@gmail.com>
2024-05-27 17:23:41 +0900

    `AnimStateView` Lua object

--- asset/script/startup.lua
+++ asset/script/startup.lua
@@ -12,7 +12,6 @@ do
     print("Skinned actor created:", e:get_id(), e:get_respath())
 end
 
---[[
 do
     bard = scene.create_skinned_actor("Sung/bard/bard_subset.dmd")
     local t = bard:get_transform()
@@ -23,11 +22,13 @@ end
 
 do
     artist = scene.create_skinned_actor("Sung/artist/artist_subset.dmd")
-    artist:set_anim_idx(10)
-    artist:get_transform():set_pos(1.35, 0, 0)
+
+    local as = artist:get_anim_state()
+    as:set_anim_idx(10)
+
     local t = artist:get_transform()
     t:set_pos(2, 0, 0)
     t:rotate(-90, 0, 1, 0)
+
     print("Skinned actor created:", artist:get_id(), artist:get_respath())
 end
-]]--
