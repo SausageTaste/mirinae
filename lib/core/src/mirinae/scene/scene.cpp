da7fa60ed642177592ebcf364cef2234f3294c06
Sungmin Woo <woos8899@gmail.com>
2024-05-31 01:47:23 +0900

    Improve shadow

--- lib/core/src/mirinae/scene/scene.cpp
+++ lib/core/src/mirinae/scene/scene.cpp
@@ -441,6 +441,30 @@ namespace { namespace scene {
 }}  // namespace ::scene
 
 
+// DLight
+namespace mirinae::cpnt {
+
+    glm::vec3 DLight::calc_to_light_dir(const glm::dmat4 view_mat) const {
+        const auto v = view_mat * transform_.make_model_mat() *
+                       glm::dvec4(0, 0, 1, 0);
+        return glm::normalize(glm::vec3(v));
+    };
+
+    glm::dmat4 DLight::make_proj_mat() const {
+        auto p = glm::orthoRH_ZO<double>(-10, 10, -10, 10, -50, 50);
+        return p;
+    }
+    glm::dmat4 DLight::make_view_mat() const {
+        return transform_.make_view_mat();
+    }
+
+    glm::dmat4 DLight::make_light_mat() const {
+        return make_proj_mat() * make_view_mat();
+    }
+
+}  // namespace mirinae::cpnt
+
+
 namespace mirinae {
 
     Scene::Scene(ScriptEngine& script) : script_(script) {
