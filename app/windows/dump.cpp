a86f0953c89f79c54705fb9c37bc0694b536fbf0
Sungmin Woo <woos8899@gmail.com>
2024-06-17 16:08:25 +0900

    Clang-format dump code

--- app/windows/dump.cpp
+++ app/windows/dump.cpp
@@ -1,9 +1,9 @@
 #include "dump.hpp"
 
-#include <tchar.h>
+#include <crtdbg.h>
 #include <dbghelp.h>
 #include <stdio.h>
-#include <crtdbg.h>
+#include <tchar.h>
 #include <cstring>
 
 
@@ -13,7 +13,15 @@ namespace {
 
     public:
         WindowsFileHandle(const char* file_name) {
-            handle_ = CreateFileA(file_name, GENERIC_READ | GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+            handle_ = CreateFileA(
+                file_name,
+                GENERIC_READ | GENERIC_WRITE,
+                0,
+                NULL,
+                CREATE_ALWAYS,
+                FILE_ATTRIBUTE_NORMAL,
+                NULL
+            );
         }
 
         ~WindowsFileHandle() {
@@ -23,24 +31,15 @@ namespace {
             }
         }
 
-        bool operator!() const {
-            return this->is_ready();
-        }
-
-        HANDLE operator*() {
-            return handle_;
-        }
-
-        bool is_ready() const {
-            return INVALID_HANDLE_VALUE == handle_;
-        }
+        bool operator!() const { return this->is_ready(); }
+        HANDLE operator*() { return handle_; }
+        bool is_ready() const { return INVALID_HANDLE_VALUE == handle_; }
 
     private:
         HANDLE handle_ = INVALID_HANDLE_VALUE;
-
     };
 
-}
+}  // namespace
 
 
 namespace mirinae { namespace windows {
@@ -48,7 +47,7 @@ namespace mirinae { namespace windows {
     void create_minidump(EXCEPTION_POINTERS* pep) {
         WindowsFileHandle file{ "dying_msg.dmp" };
         if (!file) {
-            _tprintf(_T("Minidump CreateFile failed: %u \n"), GetLastError());
+            _tprintf(_T("Minidump CreateFile failed (%u)\n"), GetLastError());
             return;
         }
 
@@ -57,18 +56,26 @@ namespace mirinae { namespace windows {
         mdei.ExceptionPointers = pep;
         mdei.ClientPointers = FALSE;
 
-        const auto mdt = (MINIDUMP_TYPE)(
-            MiniDumpWithFullMemory
-            | MiniDumpWithFullMemoryInfo
-            | MiniDumpWithHandleData
-            | MiniDumpWithThreadInfo
-            | MiniDumpWithUnloadedModules
-            );
+        const auto mdt = (MINIDUMP_TYPE)(MiniDumpWithFullMemory |
+                                         MiniDumpWithFullMemoryInfo |
+                                         MiniDumpWithHandleData |
+                                         MiniDumpWithThreadInfo |
+                                         MiniDumpWithUnloadedModules);
 
-        if (MiniDumpWriteDump(GetCurrentProcess(), GetCurrentProcessId(), *file, mdt, (pep != 0) ? &mdei : 0, 0, 0))
-            _tprintf(_T("Minidump 'dying_msg.dmp' created.\n"));
+        const auto result = MiniDumpWriteDump(
+            GetCurrentProcess(),
+            GetCurrentProcessId(),
+            *file,
+            mdt,
+            (pep != 0) ? &mdei : 0,
+            0,
+            0
+        );
+
+        if (result)
+            _tprintf(_T("Minidump 'dying_msg.dmp' created\n"));
         else
-            _tprintf(_T("MiniDumpWriteDump failed. Error: %u \n"), GetLastError());
+            _tprintf(_T("MiniDumpWriteDump failed (%u)\n"), GetLastError());
     }
 
-}}
+}}  // namespace mirinae::windows
