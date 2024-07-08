2b8f89ea86b8648a58c493c3f614fcd304c680a6
Sungmin Woo <woos8899@gmail.com>
2024-07-08 13:01:15 +0900

    Simple network communation using `ENet`

--- app/server/main.cpp
+++ app/server/main.cpp
@@ -1,4 +1,5 @@
 #include "mirinae/cosmos.hpp"
+#include "mirinae/lightweight/network.hpp"
 #include "mirinae/lightweight/script.hpp"
 
 #include "dump.hpp"
@@ -10,12 +11,17 @@ namespace {
 
     public:
         Server() {
+            server_ = mirinae::create_server();
             script_ = std::make_shared<mirinae::ScriptEngine>();
             cosmos_ = std::make_shared<mirinae::CosmosSimulator>(*script_);
         }
 
-        void do_frame() { cosmos_->do_frame(); }
+        void do_frame() {
+            server_->do_frame();
+            cosmos_->do_frame();
+        }
 
+        std::unique_ptr<mirinae::INetworkServer> server_;
         std::shared_ptr<mirinae::ScriptEngine> script_;
         std::shared_ptr<mirinae::CosmosSimulator> cosmos_;
     };
