f31e0a9d6998d31b57889b6820c68b916f29e7e5
Sungmin Woo <woos8899@gmail.com>
2024-07-19 17:32:14 +0900

    Generate mipmaps

--- script/convert_ktx.py
+++ script/convert_ktx.py
@@ -1,13 +1,15 @@
 import os
+import multiprocessing as mp
 
 import utils
 
 ROOT_DIR = utils.find_root_dir()
+DOCS_DIR = os.path.expanduser("~/Documents")
 
 
-def main():
-    src_path = os.path.join(ROOT_DIR, r"asset\textures\missing_texture.png")
-    dst_path = os.path.join(ROOT_DIR, r"asset\textures\missing_texture.ktx")
+def __do_once(src_path: str) -> str:
+    src_path = src_path
+    dst_path = os.path.splitext(src_path)[0] + ".ktx"
 
     commands = [
         "ktx",
@@ -15,12 +17,24 @@ def main():
         "--format R8G8B8_SRGB",
         "--encode uastc",
         "--assign-oetf srgb",
-        src_path,
-        dst_path,
+        "--generate-mipmap",
+        f'"{src_path}"',
+        f'"{dst_path}"',
     ]
     cmd = " ".join(commands)
-    print(cmd)
     os.system(cmd)
+    return dst_path
+
+
+def main():
+    work_list = [
+        os.path.join(ROOT_DIR, r"asset\textures\missing_texture.png"),
+        os.path.join(DOCS_DIR, r"Mirinapp\ThinMatrix\Character Texture.png"),
+    ]
+
+    with mp.Pool(mp.cpu_count()) as pool:
+        for dst_path in pool.imap_unordered(__do_once, work_list):
+            print(f"Converted: {dst_path}")
 
 
 if __name__ == "__main__":
