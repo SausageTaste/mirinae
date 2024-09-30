260d33adb24f53e3ae73c1ef78a54f117c7c0b1d
Sungmin Woo <woos8899@gmail.com>
2024-09-30 09:47:36 +0900

    More render passes into separate cpp files

---
+++ lib/vulkan/src/render/renderpass/shadow.cpp
@@ -0,0 +1,262 @@
+#include "mirinae/render/renderpass/common.hpp"
+
+
+// shadowmap
+namespace { namespace shadowmap {
+
+    VkDescriptorSetLayout create_desclayout_actor(
+        mirinae::DesclayoutManager& desclayouts, mirinae::VulkanDevice& device
+    ) {
+        return desclayouts.get("gbuf:actor").layout();
+    }
+
+    VkRenderPass create_renderpass(VkFormat depth, VkDevice logi_device) {
+        mirinae::RenderPassBuilder builder;
+
+        builder.attach_desc()
+            .add(depth)
+            .ini_layout(VK_IMAGE_LAYOUT_UNDEFINED)
+            .fin_layout(VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+            .op_pair_clear_store();
+
+        builder.depth_attach_ref().set(0);
+
+        builder.subpass_dep().add().preset_single();
+
+        return builder.build(logi_device);
+    }
+
+    VkPipeline create_pipeline(
+        VkRenderPass renderpass,
+        VkPipelineLayout pipelineLayout,
+        mirinae::VulkanDevice& device
+    ) {
+        mirinae::PipelineBuilder builder{ device };
+
+        builder.shader_stages()
+            .add_vert(":asset/spv/shadow_vert.spv")
+            .add_frag(":asset/spv/shadow_frag.spv");
+
+        builder.vertex_input_state().set_static();
+
+        builder.rasterization_state()
+            .depth_clamp_enable(device.has_supp_depth_clamp())
+            .depth_bias(80, 8);
+
+        builder.depth_stencil_state()
+            .depth_test_enable(true)
+            .depth_write_enable(true);
+
+        builder.dynamic_state().add_viewport().add_scissor();
+
+        return builder.build(renderpass, pipelineLayout);
+    }
+
+
+    class RPBundle : public mirinae::IRenderPassBundle {
+
+    public:
+        RPBundle(
+            VkFormat depth_format,
+            mirinae::DesclayoutManager& desclayouts,
+            mirinae::VulkanDevice& device
+        )
+            : IRenderPassBundle(device) {
+            formats_ = {
+                depth_format,
+            };
+
+            clear_values_.at(0).depthStencil = { 1.0f, 0 };
+
+            renderpass_ = create_renderpass(
+                formats_.at(0), device.logi_device()
+            );
+            layout_ = mirinae::PipelineLayoutBuilder{}
+                          .desc(create_desclayout_actor(desclayouts, device))
+                          .add_vertex_flag()
+                          .pc<mirinae::U_ShadowPushConst>()
+                          .build(device);
+            pipeline_ = create_pipeline(renderpass_, layout_, device);
+        }
+
+        ~RPBundle() override { this->destroy(); }
+
+        void destroy() override {
+            if (VK_NULL_HANDLE != pipeline_) {
+                vkDestroyPipeline(device_.logi_device(), pipeline_, nullptr);
+                pipeline_ = VK_NULL_HANDLE;
+            }
+
+            if (VK_NULL_HANDLE != layout_) {
+                vkDestroyPipelineLayout(
+                    device_.logi_device(), layout_, nullptr
+                );
+                layout_ = VK_NULL_HANDLE;
+            }
+
+            if (VK_NULL_HANDLE != renderpass_) {
+                vkDestroyRenderPass(
+                    device_.logi_device(), renderpass_, nullptr
+                );
+                renderpass_ = VK_NULL_HANDLE;
+            }
+        }
+
+        VkFramebuffer fbuf_at(uint32_t index) const override {
+            return VK_NULL_HANDLE;
+        }
+
+        const VkClearValue* clear_values() const override {
+            return clear_values_.data();
+        }
+
+        uint32_t clear_value_count() const override {
+            return static_cast<uint32_t>(clear_values_.size());
+        }
+
+    private:
+        std::array<VkFormat, 1> formats_;
+        std::array<VkClearValue, 1> clear_values_;
+    };
+
+}}  // namespace ::shadowmap
+
+
+// shadowmap skin
+namespace { namespace shadowmap_skin {
+
+    VkDescriptorSetLayout create_desclayout_actor(
+        mirinae::DesclayoutManager& desclayouts, mirinae::VulkanDevice& device
+    ) {
+        return desclayouts.get("gbuf:actor_skinned").layout();
+    }
+
+    VkRenderPass create_renderpass(VkFormat depth, VkDevice logi_device) {
+        mirinae::RenderPassBuilder builder;
+
+        builder.attach_desc()
+            .add(depth)
+            .ini_layout(VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL)
+            .fin_layout(VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL)
+            .op_pair_load_store();
+
+        builder.depth_attach_ref().set(0);
+
+        builder.subpass_dep().add().preset_single();
+
+        return builder.build(logi_device);
+    }
+
+    VkPipeline create_pipeline(
+        VkRenderPass renderpass,
+        VkPipelineLayout pipelineLayout,
+        mirinae::VulkanDevice& device
+    ) {
+        mirinae::PipelineBuilder builder{ device };
+
+        builder.shader_stages()
+            .add_vert(":asset/spv/shadow_skin_vert.spv")
+            .add_frag(":asset/spv/shadow_frag.spv");
+
+        builder.vertex_input_state().set_skinned();
+
+        builder.rasterization_state()
+            .depth_clamp_enable(device.has_supp_depth_clamp())
+            .depth_bias(80, 8);
+
+        builder.depth_stencil_state()
+            .depth_test_enable(true)
+            .depth_write_enable(true);
+
+        builder.dynamic_state().add_viewport().add_scissor();
+
+        return builder.build(renderpass, pipelineLayout);
+    }
+
+
+    class RPBundle : public mirinae::IRenderPassBundle {
+
+    public:
+        RPBundle(
+            VkFormat depth_format,
+            mirinae::DesclayoutManager& desclayouts,
+            mirinae::VulkanDevice& device
+        )
+            : IRenderPassBundle(device) {
+            formats_ = {
+                depth_format,
+            };
+
+            clear_values_.at(0).depthStencil = { 1.0f, 0 };
+
+            renderpass_ = create_renderpass(
+                formats_.at(0), device.logi_device()
+            );
+            layout_ = mirinae::PipelineLayoutBuilder{}
+                          .desc(create_desclayout_actor(desclayouts, device))
+                          .add_vertex_flag()
+                          .pc<mirinae::U_ShadowPushConst>()
+                          .build(device);
+            pipeline_ = create_pipeline(renderpass_, layout_, device);
+        }
+
+        ~RPBundle() override { this->destroy(); }
+
+        void destroy() override {
+            if (VK_NULL_HANDLE != pipeline_) {
+                vkDestroyPipeline(device_.logi_device(), pipeline_, nullptr);
+                pipeline_ = VK_NULL_HANDLE;
+            }
+
+            if (VK_NULL_HANDLE != layout_) {
+                vkDestroyPipelineLayout(
+                    device_.logi_device(), layout_, nullptr
+                );
+                layout_ = VK_NULL_HANDLE;
+            }
+
+            if (VK_NULL_HANDLE != renderpass_) {
+                vkDestroyRenderPass(
+                    device_.logi_device(), renderpass_, nullptr
+                );
+                renderpass_ = VK_NULL_HANDLE;
+            }
+        }
+
+        VkFramebuffer fbuf_at(uint32_t index) const override {
+            return VK_NULL_HANDLE;
+        }
+
+        const VkClearValue* clear_values() const override {
+            return clear_values_.data();
+        }
+
+        uint32_t clear_value_count() const override {
+            return static_cast<uint32_t>(clear_values_.size());
+        }
+
+    private:
+        std::array<VkFormat, 1> formats_;
+        std::array<VkClearValue, 1> clear_values_;
+    };
+
+}}  // namespace ::shadowmap_skin
+
+
+namespace mirinae {
+
+    void create_rp_shadow(
+        RpMap& out,
+        VkFormat depth_format,
+        DesclayoutManager& desclayouts,
+        VulkanDevice& device
+    ) {
+        out["shadowmap"] = std::make_unique<::shadowmap::RPBundle>(
+            depth_format, desclayouts, device
+        );
+        out["shadowmap_skin"] = std::make_unique<::shadowmap_skin::RPBundle>(
+            depth_format, desclayouts, device
+        );
+    }
+
+}  // namespace mirinae
