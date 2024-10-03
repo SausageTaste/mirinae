e01ad47890c055f3dbfadf37c6c83faa7fc2d8f5
Sungmin Woo <woos8899@gmail.com>
2024-10-04 02:46:43 +0900

    Do the same for `envmap`

--- lib/vulkan/include/mirinae/render/renderpass/envmap.hpp
+++ lib/vulkan/include/mirinae/render/renderpass/envmap.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <memory>
+
+#include "mirinae/cosmos.hpp"
 #include "mirinae/render/renderpass/common.hpp"
 
 
@@ -10,5 +13,39 @@ namespace mirinae::rp::envmap {
         DesclayoutManager& desclayouts,
         VulkanDevice& device
     );
+
+
+    class IRpMaster {
+
+    public:
+        virtual ~IRpMaster() = default;
+
+        virtual void init(
+            IRenderPassRegistry& rp_pkg,
+            TextureManager& tex_man,
+            DesclayoutManager& desclayouts,
+            VulkanDevice& device
+        ) = 0;
+
+        virtual void destroy(VulkanDevice& device) = 0;
+
+        virtual void record(
+            const VkCommandBuffer cur_cmd_buf,
+            const DrawSheet& draw_sheet,
+            const FrameIndex frame_index,
+            const CosmosSimulator& cosmos,
+            const ShainImageIndex image_index,
+            const IRenderPassRegistry& rp_pkg
+        ) = 0;
+
+        virtual VkImageView diffuse_view(size_t index) const = 0;
+        virtual VkImageView specular_view(size_t index) const = 0;
+        virtual VkImageView brdf_lut_view() const = 0;
+        virtual VkImageView sky_tex_view() const = 0;
+
+        virtual glm::dvec3& envmap_pos(size_t index) = 0;
+    };
+
+    std::unique_ptr<IRpMaster> create_rp_master();
 
 }  // namespace mirinae::rp::envmap
