628a8e7ab0352ee4f574b87a9e04a10bd2fad187
Sungmin Woo <woos8899@gmail.com>
2025-02-16 15:29:57 +0900

    Update transform imgui UI

--- lib/cosmos/src/cpnt/transform.cpp
+++ lib/cosmos/src/cpnt/transform.cpp
@@ -7,19 +7,31 @@
 namespace mirinae::cpnt {
 
     void Transform::render_imgui(const sung::SimClock& clock) {
-        auto transformf = this->copy<float>();
-        glm::vec3 rot{ 0 };
+        const float POS_SPEED = 0.1f;
+        const float ROT_SPEED = 0.1f;
+        const float SCALE_SPEED = 0.01f;
 
-        ImGui::DragFloat3("Pos", &transformf.pos_[0]);
-        ImGui::DragFloat3("Rot", &rot[0]);
+        ImGui::DragScalarN("Pos", ImGuiDataType_Double, &pos_[0], 3, POS_SPEED);
+
+        auto euler_degrees = glm::degrees(glm::eulerAngles(rot_));
+        const auto rot_updated = ImGui::DragScalarN(
+            "Rotation (XYZ)",
+            ImGuiDataType_Double,
+            &euler_degrees[0],
+            3,
+            ROT_SPEED
+        );
+        if (rot_updated)
+            rot_ = glm::dquat(glm::radians(euler_degrees));
+
+        ImGui::DragScalarN(
+            "Scale", ImGuiDataType_Double, &scale_[0], 3, SCALE_SPEED
+        );
+
         if (ImGui::Button("Reset rotation"))
-            transformf.rot_ = glm::quat(1, 0, 0, 0);
-        ImGui::DragFloat3("Scale", &transformf.scale_[0]);
-
-        this->TransformQuat<double>::operator=(transformf.copy<double>());
-        this->rotate(Angle::from_deg(rot.x), glm::vec3{ 1, 0, 0 });
-        this->rotate(Angle::from_deg(rot.y), glm::vec3{ 0, 1, 0 });
-        this->rotate(Angle::from_deg(rot.z), glm::vec3{ 0, 0, 1 });
+            rot_ = glm::dquat(1, 0, 0, 0);
+        if (ImGui::Button("Reset scale"))
+            scale_ = glm::dvec3(1);
     }
 
 }  // namespace mirinae::cpnt
