98892841b74f62a8be15deb89422f9d59685d922
Sungmin Woo <woos8899@gmail.com>
2025-02-23 23:03:32 +0900

    Update transform ImGui UI

--- lib/cosmos/src/cpnt/transform.cpp
+++ lib/cosmos/src/cpnt/transform.cpp
@@ -11,27 +11,58 @@ namespace mirinae::cpnt {
         const float ROT_SPEED = 0.1f;
         const float SCALE_SPEED = 0.01f;
 
-        ImGui::DragScalarN("Pos", ImGuiDataType_Double, &pos_[0], 3, POS_SPEED);
-
-        auto euler_degrees = glm::degrees(glm::eulerAngles(rot_));
-        const auto rot_updated = ImGui::DragScalarN(
-            "Rotation (XYZ)",
-            ImGuiDataType_Double,
-            &euler_degrees[0],
-            3,
-            ROT_SPEED
-        );
-        if (rot_updated)
-            rot_ = glm::dquat(glm::radians(euler_degrees));
-
         ImGui::DragScalarN(
-            "Scale", ImGuiDataType_Double, &scale_[0], 3, SCALE_SPEED
+            "Pos", ImGuiDataType_Double, &pos_[0], 3, POS_SPEED, 0, 0, "%.2f"
         );
 
-        if (ImGui::Button("Reset rotation"))
-            rot_ = glm::dquat(1, 0, 0, 0);
-        if (ImGui::Button("Reset scale"))
-            scale_ = glm::dvec3(1);
+        ImGui::Separator();
+
+        {
+            ImGui::DragScalarN(
+                "Quat (XYZW)", ImGuiDataType_Double, &rot_[0], 4, ROT_SPEED
+            );
+
+            glm::dvec3 ypr{ 0, 0, 0 };
+            const auto yrp_update = ImGui::DragScalarN(
+                "Yaw Pitch Roll",
+                ImGuiDataType_Double,
+                &ypr[0],
+                3,
+                ROT_SPEED,
+                0,
+                0,
+                0
+
+            );
+            if (yrp_update) {
+                this->rotate(Angle::from_deg(ypr[0]), this->make_up_dir());
+                this->rotate(Angle::from_deg(-ypr[1]), this->make_right_dir());
+                this->rotate(
+                    Angle::from_deg(-ypr[2]), this->make_forward_dir()
+                );
+            }
+
+            if (ImGui::Button("Reset rotation"))
+                rot_ = glm::dquat(1, 0, 0, 0);
+        }
+
+        ImGui::Separator();
+
+        {
+            ImGui::DragScalarN(
+                "Scale", ImGuiDataType_Double, &scale_[0], 3, SCALE_SPEED
+            );
+
+            double scale = (scale_.x + scale_.y + scale_.z) / 3.0;
+            const auto uni_scale_udpate = ImGui::DragScalar(
+                "Uniform scale", ImGuiDataType_Double, &scale, SCALE_SPEED
+            );
+            if (uni_scale_udpate)
+                scale_ = glm::dvec3(scale);
+
+            if (ImGui::Button("Reset scale"))
+                scale_ = glm::dvec3(1);
+        }
     }
 
 }  // namespace mirinae::cpnt
