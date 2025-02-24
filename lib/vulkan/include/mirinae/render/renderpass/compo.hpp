5051e6ecc6d4c1e2738142625a50e9f78fe2199c
Sungmin Woo <woos8899@gmail.com>
2025-02-24 17:29:19 +0900

    Remove obsolete rp code

--- lib/vulkan/include/mirinae/render/renderpass/compo.hpp
+++ lib/vulkan/include/mirinae/render/renderpass/compo.hpp
@@ -5,47 +5,6 @@
 
 namespace mirinae::rp::compo {
 
-    void create_rp(
-        IRenderPassRegistry& reg,
-        uint32_t width,
-        uint32_t height,
-        FbufImageBundle& fbuf_bundle,
-        DesclayoutManager& desclayouts,
-        Swapchain& swapchain,
-        VulkanDevice& device
-    );
-
-
-    class RpMasterBasic {
-
-    public:
-        void init(
-            DesclayoutManager& desclayouts,
-            FbufImageBundle& fbufs,
-            VkImageView dlight_shadowmap,
-            VkImageView slight_shadowmap,
-            VkImageView env_diffuse,
-            VkImageView env_specular,
-            VkImageView env_lut,
-            VulkanDevice& device
-        );
-
-        void destroy(VulkanDevice& device);
-
-        void record(
-            const VkCommandBuffer cur_cmd_buf,
-            const VkExtent2D& fbuf_ext,
-            const FrameIndex frame_index,
-            const ShainImageIndex image_index,
-            const IRenderPassRegistry& rp_pkg
-        );
-
-        DescPool desc_pool_;
-        std::vector<VkDescriptorSet> desc_sets_;
-        std::vector<Buffer> ubufs_;
-    };
-
-
     URpStates create_rps_dlight(
         mirinae::CosmosSimulator& cosmos,
         mirinae::RpResources& rp_res,
