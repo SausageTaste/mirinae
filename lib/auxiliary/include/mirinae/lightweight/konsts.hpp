e15e2690fc5aaa809a52916d821e2091edb78d16
Sungmin Woo <woos8899@gmail.com>
2025-02-27 10:29:03 +0900

    Minor

--- lib/auxiliary/include/mirinae/lightweight/konsts.hpp
+++ lib/auxiliary/include/mirinae/lightweight/konsts.hpp
@@ -3,7 +3,7 @@
 
 namespace mirinae {
 
-    constexpr char* const ENGINE_NAME = "mirinae";
+    const char* const ENGINE_NAME = "mirinae";
 
     constexpr int ENGINE_VERSION_MAJOR = 0;
     constexpr int ENGINE_VERSION_MINOR = 1;
