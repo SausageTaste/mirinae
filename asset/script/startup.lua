1cc609c234545a83aa18dbe84a67a12a092e2ee3
Sungmin Woo <woos8899@gmail.com>
2025-03-20 09:15:56 +0900

    Modify scene

--- asset/script/startup.lua
+++ asset/script/startup.lua
@@ -1,27 +1,13 @@
-scene = require('scene')
+local scene = require('scene')
 
 do
     local e = scene.create_static_actor("Sung/axes.dun/axes.dmd")
-    e:get_transform():set_scale(1)
     print("Static actor created:", e:get_id(), e:get_respath())
 end
 
 do
-    sponza = scene.create_static_actor("Sung/sponza.dun/sponza.dmd")
-    sponza:get_transform():set_pos(27, 1, -18)
-    sponza:get_transform():set_scale(0.01)
-    print("Static actor created:", sponza:get_id(), sponza:get_respath())
-end
-
-do
-    city = scene.create_static_actor("Sung/city.dun/city.dmd")
-    city:get_transform():set_scale(0.5)
-    print("Static actor created:", city:get_id(), city:get_respath())
-end
-
-do
-    local e = scene.create_static_actor("Sung/cerberus.dun/Cerberus_LP.dmd")
-    e:get_transform():set_pos(0.5, 2, 0.5)
+    local e = scene.create_static_actor("Sung/bistro.dun/Bistro_v5_2.dmd")
+    e:get_transform():set_pos(-100, 2, -39)
     print("Static actor created:", e:get_id(), e:get_respath())
 end
 
@@ -32,8 +18,8 @@ do
     a:set_anim_idx(0)
 
     local t = e:get_transform()
-    t:set_pos(-59.000, 7.750, -50.000)
-    t:set_scale(0.14)
+    t:set_pos(-112, 2, -39)
+    t:set_scale(0.3)
 
     print("Skinned actor created:", e:get_id(), e:get_respath())
 end
@@ -45,8 +31,9 @@ do
     a:set_anim_name("idle_normal_1")
 
     local t = e:get_transform()
-    t:set_pos(-58.300, 7.750, -50.000)
+    t:set_pos(-113, 2, -39)
     t:rotate(-90, 0, 1, 0)
+    t:set_scale(2.3)
 
     print("Skinned actor created:", e:get_id(), e:get_respath())
 end
