565d77269e9c1a895f000352baf164a933156575
Sungmin Woo <woos8899@gmail.com>
2025-03-23 16:37:02 +0900

    Update startup.lua

--- asset/script/startup.lua
+++ asset/script/startup.lua
@@ -6,6 +6,12 @@ do
 end
 
 do
+    local e = scene.create_static_actor("Sung/city.dun/city.dmd")
+    e:get_transform():set_pos(-21, 1.8, 40.7)
+    print("Static actor created:", e:get_id(), e:get_respath())
+end
+
+do
     local e = scene.create_static_actor("Sung/bistro.dun/Bistro_v5_2.dmd")
     e:get_transform():set_pos(-100, 2, -39)
     print("Static actor created:", e:get_id(), e:get_respath())
