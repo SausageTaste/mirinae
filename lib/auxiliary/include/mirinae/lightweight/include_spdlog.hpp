d5458ba6217ddaf7c47bb995abc884c8621d201e
Sungmin Woo <woos8899@gmail.com>
2025-05-09 17:00:13 +0900

    Bruh the logging macros have been completely wrong

--- lib/auxiliary/include/mirinae/lightweight/include_spdlog.hpp
+++ lib/auxiliary/include/mirinae/lightweight/include_spdlog.hpp
@@ -8,21 +8,36 @@
 #include <spdlog/spdlog.h>
 
 #define MIRINAE_ABORT(...)                         \
-    {                                              \
+    do {                                           \
         const auto msg = fmt::format(__VA_ARGS__); \
-        SPDLOG_CRITICAL(msg);                      \
+        SPDLOG_CRITICAL("{}", msg);                \
         std::abort();                              \
-    }                                              \
-    while (0)
+    } while (0)
 
-#define MIRINAE_ASSERT(cond)                    \
-    if (!(cond)) {                              \
-        MIRINAE_ABORT("Assert failed: " #cond); \
-    }                                           \
-    while (0)
+#define MIRINAE_ASSERT(cond)                        \
+    do {                                            \
+        if (!(cond)) {                              \
+            MIRINAE_ABORT("Assert failed: " #cond); \
+        }                                           \
+    } while (0)
 
-#define MIRINAE_ASSERTM(cond, ...)  \
-    if (!(cond)) {                  \
-        MIRINAE_ABORT(__VA_ARGS__); \
-    }                               \
-    while (0)
+#define MIRINAE_ASSERTM(cond, ...)      \
+    do {                                \
+        if (!(cond)) {                  \
+            MIRINAE_ABORT(__VA_ARGS__); \
+        }                               \
+    } while (0)
+
+#define MIRINAE_VERIFY(cond)                            \
+    do {                                                \
+        if (!(cond)) {                                  \
+            SPDLOG_WARN("Verification failed: " #cond); \
+        }                                               \
+    } while (0)
+
+#define MIRINAE_VERIFYM(cond, ...)    \
+    do {                              \
+        if (!(cond)) {                \
+            SPDLOG_WARN(__VA_ARGS__); \
+        }                             \
+    } while (0)
