f0c5922cfb0c71aaa3941f706ec7c26d08eebe18
Sungmin Woo <woos8899@gmail.com>
2025-06-09 23:49:17 +0900

    Slang for h0 shader

--- lib/vulkan/src/renderpass/ocean/h0.cpp
+++ lib/vulkan/src/renderpass/ocean/h0.cpp
@@ -374,7 +374,7 @@ namespace {
                     .build(pipe_layout_, device);
 
                 pipeline_ = mirinae::create_compute_pipeline(
-                    ":asset/spv/ocean_tilde_h_comp.spv", pipe_layout_, device
+                    ":asset/spv/ocean_h0_comp.spv", pipe_layout_, device
                 );
             }
 
