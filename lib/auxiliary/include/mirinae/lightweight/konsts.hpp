5e85d9639ce048df59cdeecd030eda4b9eba73b1
Sungmin Woo <woos8899@gmail.com>
2025-07-02 22:23:51 +0900

    Bump version to 0.1.1
    Updated project version in CMakeLists.txt and incremented ENGINE_VERSION_PATCH in konsts.hpp to reflect the new version 0.1.1.

--- lib/auxiliary/include/mirinae/lightweight/konsts.hpp
+++ lib/auxiliary/include/mirinae/lightweight/konsts.hpp
@@ -7,6 +7,6 @@ namespace mirinae {
 
     constexpr int ENGINE_VERSION_MAJOR = 0;
     constexpr int ENGINE_VERSION_MINOR = 1;
-    constexpr int ENGINE_VERSION_PATCH = 0;
+    constexpr int ENGINE_VERSION_PATCH = 1;
 
 }  // namespace mirinae
