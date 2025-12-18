b549748d29b50d3ed601e744964e35802d0c7b4b
Sungmin Woo <woos8899@gmail.com>
2025-12-18 10:38:40 +0900

    Remove all GLSL related codes

--- asset/glsl/utils/normal_mapping.glsl
+++
@@ -1,8 +0,0 @@
-
-mat3 make_tbn_mat(vec3 normal, vec3 tangent, mat4 m) {
-    vec3 T = normalize(vec3(m * vec4(tangent, 0)));
-    vec3 N = normalize(vec3(m * vec4(normal, 0)));
-    T = normalize(T - dot(T, N) * N);
-    vec3 B = cross(N, T);
-    return mat3(T, B, N);
-}
